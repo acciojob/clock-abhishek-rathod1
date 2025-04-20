91eebab518a05fe5f3cdf317ec40efd900bbca76
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-20 10:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+const dateContainer = document.getElementById("timer")
+
+// function renderDate() {
+// 	let date = new Date()
+// 	// let amPm = if(date.Hours)
+// 	dateContainer.innerHTML = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+// }
+
+setInterval(
+	function () {
+// renderDate();
+		let currentDate = new Date();
+
+		dateContainer.innerHTML = currentDate.toLocaleString();
+		
+	},1000
+)
+
+
